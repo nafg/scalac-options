c0ba29bf6313e17187cfc50180dffdcf4ec53c28
Naftoli Gugenheim <naftoligug@gmail.com>
2021-04-11 03:26:01 -0400

    CompilationTests

---
+++ src/test/scala/io/github/nafg/scalacoptions/CompilationTests.scala
@@ -0,0 +1,12 @@
+package io.github.nafg.scalacoptions
+
+import io.github.nafg.scalacoptions.WarningsConfig.{Category, Filter}
+
+object CompilationTests {
+  ScalacOptions.all("")(
+    (opts: options.Common) =>
+      opts.deprecation ++ opts.unchecked ++ opts.feature,
+    (_: options.V2_13).Xlint("_"),
+    WarningsConfig(Filter.Category(Category.`lint-byname-implicit`).silent)
+  )
+}
