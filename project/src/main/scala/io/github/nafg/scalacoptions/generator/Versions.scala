fae7e032fe7a70977e3f23f214d34c732cea23a6
Scala Steward <me@scala-steward.org>
2021-12-20 01:15:13 +0100

    Reformat with scalafmt 2.7.5

--- project/src/main/scala/io/github/nafg/scalacoptions/generator/Versions.scala
+++ project/src/main/scala/io/github/nafg/scalacoptions/generator/Versions.scala
@@ -3,23 +3,27 @@ package io.github.nafg.scalacoptions.generator
 import sjsonnew.BasicJsonProtocol._
 import sjsonnew.{:*:, IsoLList, LList, LNil}
 
-
 object Versions {
   val commonHelpFlags = Seq("-help", "-X", "-Y")
 
-  case class Minor(epoch: Int,
-                   major: Int,
-                   minor: Int,
-                   prerelease: Option[(String, Int)],
-                   helpFlags: Seq[String]) {
+  case class Minor(
+      epoch: Int,
+      major: Int,
+      minor: Int,
+      prerelease: Option[(String, Int)],
+      helpFlags: Seq[String]
+  ) {
     lazy val prereleaseString = prerelease.map { case (let, num) => let + num }
-    lazy val versionString = s"$epoch.$major.$minor" + prereleaseString.fold("")("-" + _)
+    lazy val versionString =
+      s"$epoch.$major.$minor" + prereleaseString.fold("")("-" + _)
 
     override def toString = versionString
   }
 
   object Minor {
-    implicit val minorLListIso: IsoLList.Aux[Minor, (Int, Int, Int) :*: Option[(String, Int)] :*: Seq[String] :*: LNil] =
+    implicit val minorLListIso: IsoLList.Aux[Minor, (Int, Int, Int) :*: Option[
+      (String, Int)
+    ] :*: Seq[String] :*: LNil] =
       LList.iso(
         { case Minor(epoch, major, minor, prerelease, helpFlags) =>
           "version" -> (epoch, major, minor) :*:
@@ -27,8 +31,12 @@ object Versions {
             "helpFlags" -> helpFlags :*:
             LNil
         },
-        { case (_, (epoch, major, minor)) :*: (_, prerelease) :*: (_, helpFlags) :*: LNil =>
-          Minor(epoch, major, minor, prerelease, helpFlags)
+        {
+          case (_, (epoch, major, minor)) :*: (_, prerelease) :*: (
+                _,
+                helpFlags
+              ) :*: LNil =>
+            Minor(epoch, major, minor, prerelease, helpFlags)
         }
       )
 
@@ -51,7 +59,11 @@ object Versions {
         majors = List(
           Major(2, 11, (0 to 12).map(Minor(2, 11, _, None, commonHelpFlags))),
           Major(2, 12, (0 to 14).map(Minor(2, 12, _, None, commonHelpFlags))),
-          Major(2, 13, (0 to 6).map(Minor(2, 13, _, None, commonHelpFlags :+ "-V" :+ "-W")))
+          Major(
+            2,
+            13,
+            (0 to 6).map(Minor(2, 13, _, None, commonHelpFlags :+ "-V" :+ "-W"))
+          )
         )
       ),
       Epoch(
@@ -60,7 +72,9 @@ object Versions {
           Major(
             3,
             0,
-            (1 to 3).map(rc => Minor(3, 0, 0, Some("RC" -> rc), commonHelpFlags)) ++
+            (1 to 3).map(rc =>
+              Minor(3, 0, 0, Some("RC" -> rc), commonHelpFlags)
+            ) ++
               (0 to 2).map(Minor(3, 0, _, None, commonHelpFlags))
           ),
           Major(3, 1, Seq(Minor(3, 1, 0, None, commonHelpFlags :+ "-W")))
