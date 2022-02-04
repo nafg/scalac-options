d47675f3237d9e090c443d31c95c2ea0d65ea25b
Naftoli Gugenheim <naftoligug@gmail.com>
2022-02-04 15:29:30 -0500

    Store versions list in versions.yaml, add 3.1.1, drop old RCs

--- project/src/main/scala/io/github/nafg/scalacoptions/generator/Versions.scala
+++ project/src/main/scala/io/github/nafg/scalacoptions/generator/Versions.scala
@@ -1,12 +1,13 @@
 package io.github.nafg.scalacoptions.generator
 
+import io.circe.yaml.parser
+import sbt.io.IO
+import sbt.io.syntax.file
 import sjsonnew.BasicJsonProtocol._
 import sjsonnew.{:*:, IsoLList, LList, LNil}
 
 
 object Versions {
-  val commonHelpFlags = Seq("-help", "-X", "-Y")
-
   case class Minor(epoch: Int, major: Int, minor: Int, prerelease: Option[(String, Int)], helpFlags: Seq[String]) {
     lazy val prereleaseString = prerelease.map { case (let, num) => let + num }
     lazy val versionString =
@@ -44,27 +45,30 @@ object Versions {
     def allMinors = majors.flatMap(_.minors)
   }
 
-  val versions =
-    List(
-      Epoch(
-        epoch = 2,
-        majors = List(
-          Major(2, 11, (0 to 12).map(Minor(2, 11, _, None, commonHelpFlags))),
-          Major(2, 12, (0 to 14).map(Minor(2, 12, _, None, commonHelpFlags))),
-          Major(2, 13, (0 to 8).map(Minor(2, 13, _, None, commonHelpFlags :+ "-V" :+ "-W")))
-        )
-      ),
-      Epoch(
-        epoch = 3,
-        majors = List(
-          Major(
-            3,
-            0,
-            (1 to 3).map(rc => Minor(3, 0, 0, Some("RC" -> rc), commonHelpFlags)) ++
-              (0 to 2).map(Minor(3, 0, _, None, commonHelpFlags))
-          ),
-          Major(3, 1, Seq(Minor(3, 1, 0, None, commonHelpFlags :+ "-W")))
-        )
-      )
+  val versions = {
+    val json =
+      parser.parse(IO.read(file("versions.yaml")))
+        .toTry.get
+    val regex = """(\d+)\.\.(\d+)""".r
+    for ((epoch, json) <- json.asObject.get.toList.sortBy(_._1.toInt)) yield Epoch(
+      epoch = epoch.toInt,
+      majors =
+        for ((major, json) <- json.asObject.get.toList.sortBy(_._1.toInt))
+          yield Major(
+            epoch = epoch.toInt,
+            major = major.toInt,
+            minors =
+              for {
+                (regex(minorFirst, minorLast), json) <- json.asObject.get.toList.sortBy(_._1.toInt)
+                minor <- minorFirst.toInt to minorLast.toInt
+              } yield Minor(
+                epoch = epoch.toInt,
+                major = major.toInt,
+                minor = minor,
+                prerelease = None,
+                helpFlags = json.asObject.get("helpFlags").get.as[Seq[String]].toTry.get
+              )
+          )
     )
+  }
 }
